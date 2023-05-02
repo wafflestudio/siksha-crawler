aefc2d475f5149b6547f010985afc6419ce27ba8
Jaewan Park <jaejae1112@gmail.com>
2023-05-02 23:49:46 +0900

    Separate crawler files by restaurant

---
+++ crawlers/vet_crawler.py
@@ -0,0 +1,25 @@
+import asyncio
+from base_crawler import *
+
+
+class VetRestaurantCrawler(RestaurantCrawler):
+    url = "http://vet.snu.ac.kr/node/152"
+    restaurant = "수의대식당"
+
+    async def run_30days(self):
+        return await asyncio.gather(self.run(), return_exceptions=True)
+
+    def crawl(self, soup, **kwargs):
+        soup.div.extract()
+        trs = soup.select("table > tbody > tr")
+
+        types = [th.text for th in trs[0].find_all("th")[1:]]
+
+        for tr in trs[1:]:
+            tds = tr.find_all("td")
+            date = tds[0].text
+            for col_idx, td in enumerate(tds[1:]):
+                meal = self.normalize(
+                    Meal(self.restaurant, td.text, date, types[col_idx])
+                )
+                self.found_meal(meal)
